bd5d0378277af137a75f54bae3b676cb207002ab
daniel2234 <daniel2234@me.com>
2018-03-19 22:48:47 -0400

    finished problem

---
+++ the_basics/exercise4.rb
@@ -0,0 +1,10 @@
+# Use the dates from the previous example and store them in an array.
+# Then make your program output the same thing as exercise 3.
+
+movie_dates = [1975, 2004, 2013, 2001, 1981]
+
+puts movie_dates[0]
+puts movie_dates[1]
+puts movie_dates[2]
+puts movie_dates[3]
+puts movie_dates[4]
