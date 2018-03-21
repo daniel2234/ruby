8c9bcdbeae94657b70cd165a5f76195b0e347ab3
daniel2234 <daniel2234@me.com>
2018-03-21 17:28:52 -0400

    solved the problems

---
+++ arrays/exercise4.rb
@@ -0,0 +1,13 @@
+# What does each method return in the following example?
+
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+#1.
+ puts arr.index(5)
+# 3
+#2.
+ #arr.index[5]
+# undefined method
+#3.
+ puts arr[5]
+# 8
---
+++ arrays/exercise6.rb
@@ -0,0 +1,12 @@
+# You run the following code...
+
+names = ['bob', 'joe', 'susan', 'margaret']
+names['margaret'] = 'jody'
+# ...and get the following error message:
+
+# TypeError: no implicit conversion of String into Integer
+#   from (irb):2:in `[]='
+#   from (irb):2
+#   from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+# What is the problem and how can it be fixed?
+# This an attempt to access the index of the array with a string instead of an integer
